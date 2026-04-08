b0409799c30a6d652d6734135c3de4920532faf5
Abraham Perez <156040006+AbeDaDev@users.noreply.github.com>
2026-04-07 22:31:11 -0700

    got the html and css mostly done

---
+++ money_tracker/src/App.jsx
@@ -0,0 +1,68 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [name,setName] = useState('');
+  const [datetime,setDatetime] = useState('');
+  const [description,setDescription] = useState('');
+  return (
+    <main>
+      <h1>$400<span>.00</span></h1>
+      <form>
+        <div>
+          <input type="text"
+                 value={name}
+                 onChange={ev => setName(ev.target.value)}
+                 placeholder={'+200 new samsung tv'}/>
+          <input value={datetime}
+                 onChange={ev => setDatetime(ev.target.value)}
+                 type="datetime-local"/>
+        </div>
+        <div>
+          <input type="text" 
+                 value={description}
+                 onChange={ev => setDescription(ev.target.value)}
+                 placeholder={'description'}/>
+        </div>
+        <button type="submit">Add new transaction</button>
+      </form>
+      <div className="transactions">
+
+        <div className="transaction">
+          <div className="left">
+            <div className="name">New Samsung TV</div>
+            <div className="description">it was time for a brand new tv</div>
+          </div>
+          <div className="right">
+            <div className="price red">-$500</div>
+            <div className="datetime">2026-12-18 15:45</div>
+          </div>
+        </div>
+
+        <div className="transaction">
+          <div className="left">
+            <div className="name">Gig job new website</div>
+            <div className="description">it was time for a brand new tv</div>
+          </div>
+          <div className="right">
+            <div className="price green">+$400</div>
+            <div className="datetime">2026-12-18 15:45</div>
+          </div>
+        </div>
+
+        <div className="transaction">
+          <div className="left">
+            <div className="name">Iphone</div>
+            <div className="description">it was time for a brand new tv</div>
+          </div>
+          <div className="right">
+            <div className="price red">-$900</div>
+            <div className="datetime">2026-12-18 15:45</div>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default App
